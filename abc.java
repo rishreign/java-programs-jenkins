101039e952a158b2f7c97869dca47ca3628e0264
rishreign <rishab2226@gmail.com>
2024-04-15 13:06:44 +0530

    Initial commit

---
+++ abc.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class abc {
+
+    public static void main(String[] args) {
+        System.out.println("this is first java program that is commited and deployed using jenkins");
+        System.out.println(new Date());
+    }
+}

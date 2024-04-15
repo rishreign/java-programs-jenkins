d10d576720b983fba7337be40bdfd5a44572a318
rishreign <rishab2226@gmail.com>
2024-04-15 13:18:43 +0530

    class name fixed

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import java.util.Date;
 
-public class abc {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("this is first java program that is commited and deployed using jenkins");
         System.out.println(new Date());
         System.out.println("hello");
+        System.out.println("class name fixed to ");
     }
 }
